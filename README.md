a68f640d3bd657de66893b673ceaecb1d064c8d4
Alfahad A <135044027+alfahad27@users.noreply.github.com>
2023-08-24 21:34:15 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Myportfolio
+https://myportfolio-5s4k.vercel.app/
---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+// Credits:
+// Technique: https://css-irl.info/css-only-slide-up-caption-hover-effect
+// Design: https://dribbble.com/shots/10951466-Social-Mobile-App
